3a9cfcb9a561781ab35bdef8170530871c9fdd3b
JM-SONG-12 <jaysong1255@gmail.com>
2026-03-23 14:21:28 +0900

    Add files via upload

---
+++ P2S.vhd
@@ -0,0 +1,38 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+
+entity P2S is
+    port (
+        nrst : in std_logic;
+        sclk : in std_logic;
+        inbits : in STD_LOGIC_VECTOR(1 downto 0);
+        outbit : out STD_LOGIC
+    );
+end P2S;
+
+architecture arch of P2S is
+signal counter : STD_LOGIC;
+
+begin
+    process (nrst, sclk)
+    begin
+    if nrst = '0' then
+        counter <= '0';
+    elsif rising_edge(sclk) then
+            counter <= not counter;
+        end if;
+    end process;
+
+    process(nrst, sclk)
+    begin
+        if nrst = '0' then
+            outbit <= '0';
+        elsif rising_edge(sclk) then
+            if counter = '1' then
+                outbit <= inbits(1);
+            else 
+                outbit <= inbits(0);
+            end if;
+        end if;
+    end process;
+end arch;
